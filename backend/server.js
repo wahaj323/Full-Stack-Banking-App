8611c92b2b88d15d3c9392471b6b5e9865ed8af3
wahaj323 <raowahaj323@gmail.com>
2025-07-25 14:58:15 +0500

    1st Test Launch

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
+// Add this at the top of your server.js
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+  process.exit(1);
+});
+
 import express from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
